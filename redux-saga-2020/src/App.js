db6056aff1a2dd04aa9122a77e4ed8aebdeb1991
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-03-25 16:42:12 +0300

    feat: add basic structure for PostForm component

--- redux-saga-2020/src/App.js
+++ redux-saga-2020/src/App.js
@@ -1,7 +1,23 @@
+import PostForm from './components/PostForm';
+
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className='container pt-3'>
+      <div className='row'>
+        <div className='col'>
+          <PostForm/>
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col'>
+          POSTS
+        </div>
+
+        <div className='col'>
+          FETCHED POSTS
+        </div>
+      </div>
     </div>
   );
 }
---
+++ redux-saga-2020/src/components/PostForm.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class PostForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>PostForm</h1>
+      </div>
+    );
+  }
+}
