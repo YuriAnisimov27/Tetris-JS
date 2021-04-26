223e6097229868820dcbf9733ae02c2a9ec18d21
Yuri Anisimov <yurianisimov27@gmail.com>
2021-04-26 13:31:02 +0300

    feat: build a menu based on dynamic data

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Welcome from "./Welcome";
+import HolelInfo from "./HolelInfo";
+
+const Main = () => {
+  return (
+    <main id="wrapper">
+      <Welcome/>
+      <HolelInfo/>
+    </main>
+  )
+}
+
+export default Main;
